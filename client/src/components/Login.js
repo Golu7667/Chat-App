136a2e11ab2893733140e7114fa9975ca1cb3773
unknown <goluchoudhary7808@gmail.com>
2023-07-30 13:05:07 +0530

    addserverfolder

---
+++ client/src/components/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, Box, Flex, Center, Text } from '@chakra-ui/react'
+
+function Login() {
+    return (
+        <>
+            <Box w="100vw" h="200vh" >
+                <Box w="100vw" h="200px" bgColor="green.800" mt="0px" zIndex="12px">
+
+                </Box>
+                <Box w="100vw" h="200px" bgColor="red" mt="0px" zIndex="-2">
+
+                </Box>
+
+                {/* <Flex alignItems="center" justifyContent="center" z="2">
+                   
+                    <Center w='1000px' h="1000px" bg='white' border="1px" borderColor="gray.400">
+                        <Text>Box 1</Text>
+                    </Center>
+                </Flex> */}
+            </Box>
+        </>
+    )
+}
+
+export default Login
