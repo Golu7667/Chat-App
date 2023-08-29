608b1d69db80b1940286541f83bf2e3d46ca9465
unknown <goluchoudhary7808@gmail.com>
2023-08-29 16:48:00 +0530

    deleted data

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,15 @@
-import React,{ useEffect} from 'react'
+import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Login from './components/Authentication/Login'
 import Signup from './components/Authentication/Signup'
-import { useNavigate } from 'react-router-dom'
 import Chatpage from './pages/Chatpage'
-import {ChatState} from './Context/ChatProvider'
 
 function App() {
- const navigate=useNavigate();
- const {user}=ChatState(); 
-  console.log(user)
-//  useEffect(()=>{
-//     if(user){
-//       navigate("/chats")
-//     }else{
-//       navigate("/login")
-//     }
-//  },[])
 
-   
   return (
 	<>
   
    <Routes>
-      {/* <Route path='/' element={<>hi</>}/> */}
      <Route path="/" element={<Login/>}/ >
      <Route path='/signup' element={<Signup/>}/>
      <Route path="/chats" element={<Chatpage/>}/>
